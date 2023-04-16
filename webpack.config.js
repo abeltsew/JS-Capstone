f1b127c48a9ce3fba310dc02c79ef7be2b044832
abeltsew <35835135+abeltsew@users.noreply.github.com>
2023-04-16 23:35:50 +0300

    Setup Linter

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,31 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
- module.exports = {
-    mode:"development",
-   entry: {
-     index: './src/index.js'
-   },
+module.exports = {
+  mode: 'development',
+  entry: {
+    index: './src/index.js',
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
     }),
   ],
-  devServer: {
-    static: './dist',
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
-  optimization: {
-    runtimeChunk: 'single',
-  },
-   output: {
-     filename: '[name].bundle.js',
-     path: path.resolve(__dirname, 'dist'),
-     clean:true,
-   },
-   module: {
+  module: {
     rules: [
       {
-        test: /\.scss$/,
-        use: [
-          'style-loader', 
-          'css-loader', 
-          'sass-loader' 
-        ]
-      }
-    ]
-  }
- };
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+};
