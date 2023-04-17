8dcc22cca543f65e3983847dca2d26f5b44214a7
abeltsew <35835135+abeltsew@users.noreply.github.com>
2023-04-17 20:53:05 +0300

    Add Test for Empty Comment List

--- __test__/commentCounter.test.js
+++ __test__/commentCounter.test.js
@@ -20,4 +20,12 @@ describe('Should render the exact comments count', () => {
 
     expect(commentsHeader.innerHTML).toEqual('comments (2)');
   });
+
+  it('Should render one comment in the list if the comment is empty', () => {
+    const comment = [];
+    const commentsHeader = document.createElement('h2');
+    updateCommentCount(comment, commentsHeader, true);
+
+    expect(commentsHeader.innerHTML).toEqual('comments (1)');
+  });
 });
