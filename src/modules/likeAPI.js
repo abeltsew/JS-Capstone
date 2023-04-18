5024930e0aefb31465440f46a9d697f1bf64cb42
hope <tasanbannu@gmail.com>
2023-04-18 09:57:27 -0400

    Add post and fetch functionality

---
+++ src/modules/likeAPI.js
@@ -0,0 +1,20 @@
+import { baseUrl, involvementApi } from '../../config/keys.js';
+const likesURL = `${baseUrl}apps/${involvementApi}/likes`;
+const postLike = async (id) => {
+    const result = fetch(likesURL, {
+      method: 'POST',
+      body: JSON.stringify({item_id: id,}),
+      headers: { 'Content-Type': 'application/json' },
+    });
+    const res = await result.json();
+    return res;
+  };
+
+  const fetchLikes = async () => {
+    const result = await fetch(likesURL, {
+      method: 'GET',
+    });
+    return result.json();
+};
+
+  export {postLike,fetchLikes}
