1ee6dfddbb25576c1f237fe2f6d8a8374b69e4e0
hope <tasanbannu@gmail.com>
2023-04-18 09:55:33 -0400

    Add like post functionality to our API

--- src/modules/render.js
+++ src/modules/render.js
@@ -1,9 +1,11 @@
-import renderComment from './renderComment.js';
 
-const main = document.querySelector('.main');
-const renderMeal = (item) => {
-  const div = document.createElement('div');
-  div.classList.add('list');
+import renderComment from "./renderComment.js";
+import { postLike } from "./likeAPI.js";
+const main = document.querySelector(".main");
+const renderMeal = async (item) => {
+  const div = document.createElement("div");
+  div.classList.add("list");
+
   div.innerHTML = `
       <div class="image">
       <img
@@ -16,20 +18,29 @@ const renderMeal = (item) => {
      
       </h5>
       
-        <button class="like-btn" id= "${item.idMeal}">
-           <i class="far fa-heart fa-2x"></i><span>10</span>
+             <button class="btn-like" id= "${item.idMeal}">
+             <i class="far fa-heart fa-2x"></i><span id= "likes${item.idMeal}">10</span>
         </button>
   
     </div>
     <button class="like" data-id= "${item.idMeal}">See More</button>
   `;
   main.append(div);
+  // Add event listener to the like button
+  const likeBtn = div.querySelector(".btn-like");
+  likeBtn.addEventListener("click", () => {
+    const itemId = likeBtn.getAttribute("id");
+    const likeCount = parseInt(likeBtn.querySelector("span").textContent);
+    likeBtn.querySelector("span").textContent = likeCount + 1;
+
+    postLike(itemId);
+  });
 
   const detailsBtn = document.querySelector(`[data-id= "${item.idMeal}"]`);
 
-  detailsBtn.addEventListener('click', async () => {
+  detailsBtn.addEventListener("click", async () => {
     const response = await fetch(
-      `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${item.idMeal}`,
+      `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${item.idMeal}`
     );
     const result = await response.json();
 
@@ -37,6 +48,4 @@ const renderMeal = (item) => {
   });
 };
 
-
-
-export default renderMeal;
+export { renderMeal};
