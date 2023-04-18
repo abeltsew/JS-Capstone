032552e86b8b682806234ed856b73be1aa95d3c9
hope <tasanbannu@gmail.com>
2023-04-18 11:40:13 -0400

    Fix eslint error

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import "./style.scss";
-import "./style.css";
+import './style.scss';
+import './style.css';
 
-import { renderLike, renderMeal } from "./modules/render.js";
+import { renderLike, renderMeal } from './modules/render.js';
 
-const mainAPI = "https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood";
-const itemCounter = document.querySelector("#counter");
-window.addEventListener("DOMContentLoaded", () => {
+const mainAPI = 'https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood';
+const itemCounter = document.querySelector('#counter');
+window.addEventListener('DOMContentLoaded', () => {
   fetch(mainAPI)
     .then((response) => response.json())
     .then((data) => {
