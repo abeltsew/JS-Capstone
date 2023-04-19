38b942db1e22ab8be14ff0837ccf230797293144
abeltsew <35835135+abeltsew@users.noreply.github.com>
2023-04-19 21:54:12 +0300

    Add Counter Function

--- __test__/commentCounter.test.js
+++ __test__/commentCounter.test.js
@@ -2,7 +2,7 @@
  * @jest-environment jsdom
  */
 
-import { updateCommentCount } from '../src/modules/renderComment.js';
+import updateCommentCount from '../src/modules/updateCommentCount.js';
 
 describe('Should render the exact comments count', () => {
   it('Should render one comment in the list', () => {
---
+++ src/modules/updateCommentCount.js
@@ -0,0 +1,14 @@
+let commentList = [];
+export default (comment, commentsHeader, add) => {
+  if (add) {
+    if (comment.length === 0) {
+      commentsHeader.innerHTML = `comments (1)`;
+    } else {
+      commentList.push(comment);
+      commentsHeader.innerHTML = `comments (${commentList.length})`;
+    }
+  } else {
+    commentList = [...comment];
+    commentsHeader.innerHTML = `comments (${comment.length})`;
+  }
+};
