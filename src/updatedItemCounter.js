7ab0c9e1e8479f1b128b22511d84e3e0bfaa4d04
hope <tasanbannu@gmail.com>
2023-04-19 02:34:45 -0400

    Update item counter function for empty array

--- src/updatedItemCounter.js
+++ src/updatedItemCounter.js
@@ -1,5 +1,8 @@
 const itemCounters = (items, itemCounter) => {
+  if (items.length === 0) {
+    return;
+  }
   itemCounter.innerHTML = `Food(${items.length})`;
 };
 
-export default itemCounters
+export default itemCounters;
